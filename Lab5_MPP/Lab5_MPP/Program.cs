8298c494270c08e326f5ed7b263a6ceb0db0b510
Equinox <sovereign.stas@yandex.ru>
2017-09-28 02:06:36 +0300

    Delegate function changed.

--- Lab5_MPP/Lab5_MPP/Program.cs
+++ Lab5_MPP/Lab5_MPP/Program.cs
@@ -7,6 +7,24 @@ namespace Lab5_MPP
 
     class Program
     {
+       
+        static void Func()
+        {
+            object locker = new object();
+            int time_to_sleep = 0;
+            lock (locker)
+            {
+                var rand = new Random((int)DateTime.Now.Ticks);
+
+                time_to_sleep = rand.Next(1000, 5000);
+
+                Console.WriteLine("Процесс заморожен на {0}", time_to_sleep/1000);
+                Thread.Sleep(time_to_sleep);
+            }
+            Console.WriteLine("Процесс возобновлён после паузы в {0}", time_to_sleep / 1000);
+
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Введите количество потоков - ");
@@ -14,16 +32,7 @@ namespace Lab5_MPP
             Console.Write("\n");
             MyDelegate[] del_arr = new MyDelegate[n];
             for (int i = 0; i < n; i++)
-                del_arr[i] = () => 
-                {
-                    Console.WriteLine("Информация об исполняющем потоке:");
-                    Console.WriteLine("Организационный ID потока - " + Thread.CurrentThread.ManagedThreadId);
-                    Console.WriteLine("Приоритет потока - " + Thread.CurrentThread.Priority);
-                    Console.WriteLine("Состояние потока - " + Thread.CurrentThread.ThreadState);
-                    Console.WriteLine("Поток фоновый - " + ((Thread.CurrentThread.IsBackground) ? "Да":"Нет"));
-                    Console.WriteLine("\n");
-                    Thread.Sleep(20);
-                };
+                del_arr[i] = Func;
             Parallel.WaitAll(del_arr);
             Console.WriteLine("Потоки отработали");
             Console.ReadKey();
